bd5261d10db4b084aa207efd2f377afda81ff58d
Tonmoy-Roy <emonismail69@gmail.com>
2022-08-28 14:32:12 +0600

    first commit

---
+++ js/countris.js
@@ -0,0 +1,43 @@
+const  loadCountries=()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(data=>displayCountries(data));
+
+}
+const displayCountries=countries=>{
+   const countriesContainer=document.getElementById("countries-container"); 
+
+   countries.forEach(country=> {
+
+    const countryDiv = document.createElement("div");
+    countryDiv.classList.add ( 'country');
+   
+    countryDiv.innerHTML = `
+    <h3> Country Name: ${country.name.common}</h3>
+    <p> Capital : ${country.capital ? country.capital[0]:"no capital"}</p>
+    <button onclick="loadCountryDetail('${ country.cca2}') "> details </button>
+    `
+    countriesContainer. appendChild(countryDiv);
+} )
+}
+loadCountryDetail =( code) =>{
+    const url=`
+    https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country details', code);
+    fetch(url)
+        .then(res=>res.json())
+        .then(data=>displayCountryDetail(data[0]));
+       
+}
+const displayCountryDetail =country =>{
+    console.log(country);
+const countryDetail=document.getElementById('country-detail');
+countryDetail.innerHTML = `
+<h3> details: ${country.name.common}  </h3>
+<img src="${country.flags.png} ">
+`
+
+}
+
+
+loadCountries()
